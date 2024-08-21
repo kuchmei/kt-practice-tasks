0607004d878f4452b6ed0788a6e121dde3174d93
Laptop <karishabodnar99@gmail.com>
2024-08-21 21:24:37 +0300

    Add warm-up exercise
    (cherry picked from commit a49ceb5a63d439ba6e9c448f6c8ed9eed528d2a3)

---
+++ src/main/java/com/deliberate/practice/lesson2/warmup/MultiThreadedSum.java
@@ -0,0 +1,17 @@
+package com.deliberate.practice.lesson2.warmup;
+
+import com.deliberate.practice.exception.ExerciseNotCompletedException;
+
+public class MultiThreadedSum {
+
+    /**
+     * Calculates the sum of an array of integers using two threads.
+     *
+     * @param array The array of integers to sum.
+     * @return The total sum of the array.
+     */
+    public int calculateSum(int[] array) {
+        // Implementation to be added
+        throw new ExerciseNotCompletedException();
+    }
+}
